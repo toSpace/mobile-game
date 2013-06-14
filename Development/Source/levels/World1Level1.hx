a49b2de1f43b8c59fcb3df218ae47d23487d475b
Jesse v R <jvanrheenen@gmail.com>
2013-06-14 11:35:52 +0200

    Alle XML level1 toegevoegd

--- Development/Source/levels/World1Level1.hx
+++ Development/Source/levels/World1Level1.hx
@@ -13,6 +13,10 @@ class World1Level1 extends Level {
 		var fg1_2 = new DrawObject('Specific/Level1/Environment/Foreground/FG1/fg1_2.xml');
 		var fg1_3 = new DrawObject('Specific/Level1/Environment/Foreground/FG1/fg1_3.xml');
 		var fg1_4 = new DrawObject('Specific/Level1/Environment/Foreground/FG1/fg1_4.xml');
+		var fg1_5 = new DrawObject('Specific/Level1/Environment/Foreground/FG1/fg1_5.xml');
+		var fg1_6 = new DrawObject('Specific/Level1/Environment/Foreground/FG1/fg1_6.xml');
+		var fg1_7 = new DrawObject('Specific/Level1/Environment/Foreground/FG1/fg1_7.xml');
+		var fg1_8 = new DrawObject('Specific/Level1/Environment/Foreground/FG1/fg1_8.xml');
 
         var pika = new DrawObject('test2.xml');
         // var useless = new DrawObject('test.xml');
